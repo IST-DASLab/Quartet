d1202ba7e3683dd5fa75a4157df48c22b643fc54
Andrei Panferov <andrei@panferov.org>
2025-10-26 19:59:59 +0300

    Delete quartet_qat directory. Functionality in `fp_quant`

--- quartet_qat/src/quartet_qat/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-quartet_qat: A Python package for quartet-qat
-""" 
-
-from .utils import QuartetDtype, replace_with_quartet_linear, QuartetConfig
-from .module import QuartetLinear
--- quartet_qat/src/quartet_qat/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .replace import replace_with_quartet_linear
-from .config import QuartetConfig, QuartetDtype
